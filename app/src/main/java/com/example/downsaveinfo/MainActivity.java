cd919dec14554fe0f7d80336168900d1556a953e
FlavioFMBorges <flyfmb@hotmail.com>
2023-05-24 16:24:53 -0300

    Corrigido app fechando sozinho(prob em manifest) close #5

--- app/src/main/java/com/example/downsaveinfo/MainActivity.java
+++ app/src/main/java/com/example/downsaveinfo/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.downsaveinfo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -10,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -17,13 +17,11 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    // 1 - variável EditText no Java para captura do conteúdo inserido pelo usuário (associada ao componente EditText "editTextXML" no Layout)
+
+    // 1- variável EditText no Java para captura do conteúdo inserido pelo usuário (associada ao componente EditText "editTextXML" no Layout)
     private EditText mensagemInserida;
 
-    // 8 - variável button no JAVA associada ao componente Button "downloadXML" no Layout
-    private Button downloadButton;
-
-    // 2 - String contendo a mensagem a ser enviada.
+    // 2- String contendo a mensagem a ser enviada.
     private String mensagem;
 
     @Override
@@ -31,115 +29,117 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 3 - Associando a variável mensagemInserida do Java com o componente EditText do arquivoXML
+        // 3- Associando o variável mensagemInserida do Java com o componente EditText do arquivoXML
         mensagemInserida = findViewById(R.id.editTextXML);
 
-        // 9 - Associando a variável mensagemInserida do Java com o componente EditText do arquivoXML
-        downloadButton = findViewById(R.id.downloadXML);
+        // 9- Associando o variável mensagemInserida do Java com o componente EditText do arquivoXML
+        // 8- variável Button no Java  associada ao componente Button "downloadXML" no Layout)
+        Button downloadButton = findViewById(R.id.downloadXML);
 
-        // 10 - Criando o método onClick associado ao botão para vizualização do Download
+        //10 - criando o método onClick associado ao botão para visualiação do Download.
         downloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                AnalisadorXml analisadorXml = new AnalisadorXml(mensagem);
+                analisadorXml.process();
+                mensagem = analisadorXml.getConteudo();
                 mensagemInserida.setText(mensagem);
             }
         });
 
-        // 7 - Criando uma instância para AsyncTask e executando uma tarefa executando o método execute().
+        //7 - Criando uma Instância da AsyncTask e executando a tarefa utilizando o método execute().
         MinhaAsync minhaAsync = new MinhaAsync();
         minhaAsync.execute("https://www.w3schools.com/xml/note.xml");
 
     }
+    // 4- criando o método para envido da mensagem para a próxima tela
+    public void disparoNovaTela(View v) {
 
-    // 4 - Criando o método para envio da mensagem para a próxima tela
-    public void disparoNovaTela(View v)  {
+        //4.1 atribuição do valor digitado pelo usuário no campo do EditTexXML para a variável mensagem
 
-        // 4.1 - atribuição do valor ditado pelo usuário ao campo do EditTextXML para a variável mensagem
         mensagem = mensagemInserida.getText().toString();
 
-        // 4.2 - Criação do Intent para chamada da segunda tela com envio da mensagem
+        //4.2 Criação da Intent para chamada da segunda tela com envio da mensagem.
         Intent myIntent = new Intent(this, Tela2.class);
 
-        // 4.3 - Uso do método putExtra para envio da mensagem.
+        //4.3 Uso do método putExtra para envio da mensagem.
         myIntent.putExtra("mensagemEnviada", mensagem);
 
+        //4.4 Envio da solicitação
         startActivity(myIntent);
 
     }
+    // 5 - Criando o método para realizar o download do conteúdo XML do site, utilizando a classe HttpURLConnection.
 
-    // 5 - Método para download do conteudo XML do site, utilizando a classe httURLCnnection
     private String downloadXMLFile(String theUrl){
-        try{
-            //cria uma instancia da classe URL com a url que será utilizada na conexão.
+        try {
+            //cria um instância da classe URL com a url que será utilizada na conexão.
             URL myUrl = new URL(theUrl);
 
             //abre a conexão
             HttpURLConnection myconnection = (HttpURLConnection) myUrl.openConnection();
 
-            //verifica se foi bem sucedida a conexão e exibe no layout; se sim código no 200
+            //verifica se foi bem sucedida a conexão e exibe no Logcat; se sim código 200
             int response = myconnection.getResponseCode();
-            Log.d("Download", "the response code is" + response);
+            Log.d("Download", "The response code is " + response);
 
-            //Cria a variável "data" para receber a stream de bytes.
+            // cria a variável "data" que para receber o stream de bytes
             InputStream data = myconnection.getInputStream();
 
-            //utiliza a classe InputStreamReader para converter bytes em chars.
+            //usa a classe InputStreamReader para converter bytes em chars
             InputStreamReader caracteres = new InputStreamReader(data);
 
-            //criação de um Array de char para leitura de 500 em 500 caracteres
-            char[] InputBuffer = new char[500];
+            //criação de um array de char para leitura de 500 em 500 caracteres
+            char[] inputBuffer = new char[500];
 
             //Criação de uma instância StringBuilder para formar a String final de interesse
             StringBuilder tempBuffer = new StringBuilder();
 
-            //variável para contagem de número de caracteres lidos dentro de um laço while para formação da String
+            //variável para contagem de número de caracteres lidos dentro do laço while para formação da String
             int charRead;
 
-            //laço de formação e leitura da string
-            while (true){
-                //lê os caracteres com tamanho máximo de InputBuffer(500) e informa o número lido
-                charRead = caracteres.read(InputBuffer);
-
-                // se -1, não há caracteres lidos e sai do laço.
+            //laço de leitura e formação da string
+            while (true) {
+                //lê os caracteres com tamanho máximo de inputBuffer(500) e informa o número lido
+                charRead =caracteres.read(inputBuffer);
+                //se -1 não há caracteres lidos e sai do laço.
                 if (charRead <= 0) {
                     break;
                 }
-                tempBuffer.append(String.copyValueOf(InputBuffer,0, charRead));
+                tempBuffer.append(String.copyValueOf(inputBuffer, 0, charRead));
+
             }
             return tempBuffer.toString();
         }
-        // com os métodos open.Connection(), getResponsecode() e getInputStream() podem lançar exceções de 10
-        // pois interagem com recursos externos do aplicativo, precisamos de um bloco catch para pegar eventuais exceções.
-        catch (IOException e)
+        //como as os métodos openConnection(), getResponseCode() e getInputStream() podem lançar exceções de IO
+        //pois interagem com recursos externos ao aplicativo, precisamos de um bloco cath para capturar eventuais exceções.
+        catch(IOException e)
         {
-            Log.d("Download", "Io Expection durante a conexão" + e.getMessage());
+            Log.d("Download", "IO Expection durante a conexão: " + e.getMessage());
         }
-        //nesse caso o método retorna null.
+        //e nesse caso o método retorna null.
         return null;
     }
 
-    //Precisamos dar permissão de acesso a internet ao arquivo manifest
-    // <uses-permission android:name="android.pemission.INTERNET"></uses.permission>
+    //Precisamos dar permissão de acesso a internet ao aplicativo no arquivo Manifest
+    //   <uses-permission android:name="android.permission.INTERNET"></uses-permission>
 
-    //vamos criar o código para a AsyncTask.
-    //que através de uma thread paralela fará a chamada de um método DownloadXMLFile para baixarmos o conteúdo do Feed
+    //6 - Vamos criar a classe  que é uma AsynkTask,
+// que através de uma thread paralela fará a  chamada do método DownloadXMLFile para baixarmos o conteúdo do Feed
 
-    private class MinhaAsync extends AsyncTask<String, Void, String>{
+    private class MinhaAsync extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... params) {
 
-            //vamos evocar o método downlodXMLFile passando a url como parametro
+            //vamos efocar o método downloadXMLFile passando a url como parâmetro
             mensagem = downloadXMLFile(params[0]);
 
-            //Aviso para ser exibido no Logcat caso tenha havido algum problema no download
-            if (mensagem == null){
+            //Aviso para ser exibido no Logcat caso tenha havido problema no downlod
+            if (mensagem == null) {
                 Log.d("Download", "Erro downloading");
             }
             return mensagem;
         }
     }
-
-
-
 }
